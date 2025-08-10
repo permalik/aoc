55c052fed8ea6c881f5cf2cda3cfd258d871e7d0
permalik <permalik@protonmail.com>
2025-08-09 23:38:15 -0500

    feat: handle graceful destroy

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 
-cd ..
-rm -rf aoc-python
-rm -rf aoc-c
-rm -rf aoc-zig
+repos=(
+    "/home/parallels/Docs/Git/aoc-python"
+    "/home/parallels/Docs/Git/aoc-c"
+    "/home/parallels/Docs/Git/aoc-zig"
+)
+
+commit_msg="Pre-removal auto-commit"
+
+for repo in "${repos[@]}"; do
+    echo "Process: $repo"
+    if [ ! -d "$repo/.git" ]; then
+        echo "  Skipped. Not a git repo"
+        continue
+    fi
+
+    cd "$repo" || { echo "  Failed entry: $repo"; continue; }
+
+    if [[ -n $(git status --porcelain) ]]; then
+        echo "  Detected changes: Add, Commit, and Push"
+
+        git add -A
+        git commit -m "$commit_msg"
+
+        branch=$(git symbolic-ref --quiet --short HEAD || echo "main")
+
+        git push origin "$branch"
+        if [ $? -ne 0 ]; then
+            echo "  Failed to push changes in $repo. Abort removal"
+            cd - >/dev/null
+            continue
+        fi
+    else
+        echo "  No changes. Remove directory"
+    fi
+
+    cd - > /dev/null
+    echo "  Remove directory: $repo"
+    rm -rf "$repo"
+done
+
