5dabd4e08fb4ec8bfe6729f5595748563f17c0d9
permalik <permalik@protonmail.com>
2025-09-12 18:41:09 -0500

    feat: impl lua

---
+++ aoc-lua/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "aoc-lua";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.alejandra
+            pkgs.lua
+          ];
+
+          shellHook = ''
+            cd aoc-lua
+
+            . .bashrc
+          '';
+        };
+      }
+    );
+}
